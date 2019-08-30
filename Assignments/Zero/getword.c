b167c0bfb0f8220c70f708659c0fc279560b5430
Jan Domingo <janaarondomingo@gmail.com>
2019-08-29 23:25:12 -0700

    getword.c added

---
+++ Assignments/Zero/getword.c
@@ -0,0 +1,14 @@
+//
+//  getword.c
+//  Assignments
+//
+//  Created by Jan  Domingo on 8/29/19.
+//  Copyright © 2019 Jan  Domingo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int getword(char *w)
+{
+    
+}
