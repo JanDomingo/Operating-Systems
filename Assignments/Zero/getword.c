de7a03c484aa3eabfed77ccc82247109c5420f3e
Jan Domingo <janaarondomingo@gmail.com>
2019-08-31 16:55:07 -0700

    User input and output working. Need to work on edge cases now

--- Assignments/Zero/getword.c
+++ Assignments/Zero/getword.c
@@ -1,14 +1,60 @@
 //
-//  getword.c
-//  Assignments
+//  Name: Jan Domingo
+//  RedID: 820092657
+//  Class Account: CSSC0034
+//  Course: CS570 Operating Systems
+//  Section: 1
+//  File name: getword.c
+//  Compiler Version: XCode 10.2.1
 //
-//  Created by Jan  Domingo on 8/29/19.
-//  Copyright © 2019 Jan  Domingo. All rights reserved.
-//
+//  This function examines successive words on the input stream (stdin) and returns the size of each word
+//  of the user input and adds each character into the w pointer.
 
 #include <stdio.h>
 
 int getword(char *w)
 {
+    int iochar;
+    int wordSize = 0;
     
+    //Iterates through stdin, analyzing each char of the user input
+     while ((iochar = getchar()) != EOF && iochar != '\n')
+     {
+        if (iochar == '\n' && wordSize == 0)
+            return 0;
+        
+        if (iochar == EOF && wordSize == 0)
+            return -1;
+         
+         if (iochar != ' ')
+            wordSize++;
+            *w = iochar;        //Populates the string array with characters
+            w++;                //Increments the pointer to populate the next element of the string array
+        
+         if (iochar == ' ')
+             break;
+     }
+    
+    
+    return wordSize;
 }
+    
+    
+    
+    
+    //Gets one word from the input stream
+    //A word is anything except space, newline, and EOF
+    //Input is a pointer to the beginning of a character array
+    //Output is -1, 0, or the number of characters in the word
+    
+
+    
+    //if you have collected the word 'done' then return -1 instead of 4
+    //skips leading spaces
+
+    /*if wordsize = 0 and EOF then return -1*/
+    /*if newline encountered while the wordsize is zero then return 0*/
+
+    //Put the word back into the pointer of w
+    
+    
