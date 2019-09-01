c53a5640c4b2e5374939d36c20530e710241f8a5
Jan Domingo <janaarondomingo@gmail.com>
2019-08-31 22:47:56 -0700

    Fixed magic numbers

--- Assignments/Zero/getword.c
+++ Assignments/Zero/getword.c
@@ -14,56 +14,63 @@
 
 #include <stdio.h>
 #include <string.h>
+#include "getword.h"
+
+
+#define NEWLINE '\n'
+#define FINISH -1
+#define EMPTY 0
+#define CHANGED 1
 
 static int newlineFlag = 0;
 int getword(char *w)
 {
     int iochar;
-    int wordSize = 0;
+    int wordSize = EMPTY;
     
     //Saves a pointer to the start of the array to strcmp with 'done' at the end
     char *startOfWordPtr = w;
     
     //Clears the word array so that it does not display previous characters
-    memset(startOfWordPtr, 0, 255);
+    memset(startOfWordPtr, EMPTY, STORAGE);
     
     //Iterates through stdin, analyzing each char of the user input
-     while ((iochar = getchar()) != EOF && iochar != '\n') {
+     while ((iochar = getchar()) != EOF && iochar != NEWLINE) {
          if (iochar != ' ') {
             wordSize++;
-            *w = iochar;        //Populates the string array element with characters
-            w++;                //Increments the pointer to populate the next element of the string array
+            *w = iochar;          //Populates the string array element with characters
+            w++;                  //Increments the pointer to populate the next element of the string array
          }
          
          //If it is a leading space, then it gets ignored and delimited
          //Else if it is a space between words, then it returns the size of the word
-         if (iochar == ' ' && wordSize == 0) {
+         if (iochar == ' ' && wordSize == EMPTY) {
              continue;
-         } else if (iochar == ' ' && wordSize > 0)
+         } else if (iochar == ' ' && wordSize > EMPTY)
          {
-             w--;               //Moves the pointer back one to eliminate an extra space printed
+             w--;                 //Moves the pointer back one to eliminate an extra space printed
              return wordSize;
          }
      }
     
     //Special case for the word 'done' inputted into the io stream
-    if (strcmp(startOfWordPtr, "done") == 0) {
-        return -1;
+    if (strcmp(startOfWordPtr, "done") == EMPTY) {
+        return FINISH;
     }
     
-    if (iochar == EOF && wordSize == 0)
-        return -1;
+    if (iochar == EOF && wordSize == EMPTY)
+        return FINISH;
     
-    if (iochar == '\n' && newlineFlag == 0)
+    if (iochar == NEWLINE && newlineFlag == EMPTY)
     {
-        ungetc(iochar, stdin);  //Keeps the newline character for the next getword call to print out
+        ungetc(iochar, stdin);    //Keeps the newline character for the next getword call to print out
         newlineFlag++;
         return wordSize;
     }
     
-    if (iochar == '\n' && newlineFlag == 1)
-        newlineFlag = 0;        //Resets the flag for the next user input
-        return 0;
+    if (iochar == NEWLINE && newlineFlag == CHANGED)
+        newlineFlag = EMPTY;     //Resets the flag for the next user input
+        return EMPTY;
 
     return wordSize;            //Allows user to keep inputting more values until ctrl+d is inputted
 }
