4c55680963306ba12fd587327b85f4243e9b5941
Jan Domingo <janaarondomingo@gmail.com>
2019-09-11 00:07:36 -0700

    Greedy algorithm partly working

--- Assignments/One/getword.c
+++ Assignments/One/getword.c
@@ -24,29 +24,98 @@
 
 #define FINISH -1
 #define EMPTY 0
+#define NOT_META 0
+#define IS_META 1
 #define CHANGED 1
 #define DELIMITER ' '
 #define NEWLINE '\n'
 
-int metaCharacterCheck(int iochar) {
-    char metaString[STORAGE] = {EMPTY};
-    char *startOfMetaStringPtr = metaString;
-    //Implement greedy algorithm below
-    while (iochar == '>' || iochar == '|' || iochar == '#' || iochar == '&')
-    {
-        *metaString = iochar;
-        iochar = getchar();
-        return 1;
-    }
-    
-    return 0;
+//TODO: NEED TO IMPLEMENT ANOTHER METACHARACTER CHECK THAT ONLY RETURNS A 1 OR 0
+
+//Returns either a 0 or a 1 depending on if the iochar detected is a metacharacter
+static int metaCharacterCheck(int iochar) {
+    if (iochar == '>' ||  iochar == '|' || iochar == '#' || iochar == '&') {
+        //ungetc(iochar, stdin);
+        return IS_META;
+    } else
+        return NOT_META;
 }
 
+//Returns the metacharacter word size and puts the metacharacters on the w pointer to output
+static int greedyAlgorithm(int iochar, char *w, char *wstart) {
+    //char metaString[STORAGE] = {EMPTY};
+    //char *metaStringPtr = metaString;
+    //char *startOfMetaStringPtr = metaStringPtr;
+    int metaCharWordSize = EMPTY;
+    
+    if (iochar == '>') {            //if '>'
+        *w = iochar;
+        w++;
+        metaCharWordSize++;
+        iochar = getchar();
+        
+         if (iochar == '>') {       //if '>>'
+             *w = iochar;
+             w++;
+             metaCharWordSize++;
+             iochar = getchar();
+            
+            if (iochar == '^') {    //if '>>^'
+                *w = iochar;
+                w++;
+                metaCharWordSize++;
+                iochar = getchar();
+                return metaCharWordSize;
+            }
+            return metaCharWordSize;
+         }
+        
+        if (iochar == '&') {        //if '>&'
+            *w = iochar;
+            w++;
+            metaCharWordSize++;
+            //iochar = getchar();
+            return metaCharWordSize;
+        }
+        
+        return metaCharWordSize;
+    }
+    
+    if (iochar == '|') {            //if '|'
+        *w = iochar;
+        w++;
+        metaCharWordSize++;
+        //iochar = getchar();
+        return metaCharWordSize;
+    }
+    
+    if (iochar == '#') {            //if '#'
+        *w = iochar;
+        w++;
+        metaCharWordSize++;
+        //iochar = getchar();
+        return metaCharWordSize;
+    }
+    
+    if (iochar == '&') {            //if '&'
+        *w = iochar;
+        w++;
+        metaCharWordSize++;
+        //iochar = getchar();
+        return metaCharWordSize;
+    }
+    
+    
+    
+    
+    
+    return metaCharWordSize;
+}
 
 
 int getword(char *w)
 {
-    char metaCharacters[7][3] = {">", ">>", ">&", ">>^", "|", "#", "&"};
+    //char metaCharacters[7][3] = {">", ">>", ">&", ">>^", "|", "#", "&"};
     int iochar;
     int wordSize = EMPTY;
     static int endOfFileTrigger = EMPTY;
@@ -59,12 +128,27 @@ int getword(char *w)
     
     //Iterates through stdin, analyzing each char of the user input
     while ((iochar = getchar()) != EOF) {
-        if (metaCharacterCheck(iochar) == 0) {
             wordSize++;
             *w = iochar;            //Populates the w string array element with characters
             w++;                    //Increments the pointer to populate the next element of the string array
+        
+        /*********************THIS SECTION CHECKS IF THE CHARACTER IS A META CHARACTER**********************/
+        //If the character is a meta character and there is a current word, then return the current wordsie
+        //and ungetc will restore the stdin stream to the metachar for the next run
+        if (metaCharacterCheck(iochar) == IS_META) {
+            wordSize--;              //Meta character char not counted in wordSize - has separate function
+            w--;                     //Points to the meta character
+            *w = EMPTY;              //Deletes the meta character
+            
+            if (wordSize > EMPTY) {
+                //ungetc(iochar, stdin);
+                return wordSize;
+            } else if (wordSize == EMPTY) {
+                return greedyAlgorithm(iochar, w, startOfWordPtr);
+            }
         }
         
+        /**********************THIS SECTION CHECKS IF THE CHARACTER IS A SPACE*******************************/
         //If it is a leading space, then it gets ignored and delimited
         //Else if it is a space between words, then it returns the size of the word
         if (iochar == DELIMITER && wordSize == EMPTY) {
@@ -77,6 +161,7 @@ int getword(char *w)
             return wordSize;
         }
         
+        /**********************THIS SECTION CHECKS IF THE CHARACTER IS A NEWLINE*****************************/
         //If the character is a newline directly at the end of a string
         if (iochar == NEWLINE) {
             wordSize--;              //Newline char not counted in wordSize
@@ -97,6 +182,7 @@ int getword(char *w)
         }
     }
     
+    /****************************THIS SECTION HANDLES AN EOF CHARACTER***************************************/
     //Handles premature EOF statements by saving the EOF in the stream then returning the wordSize
     if ((iochar == EOF) && (endOfFileTrigger == EMPTY))
     {
@@ -112,5 +198,3 @@ int getword(char *w)
     }
     return FINISH;
 }
-
-
