7cc8dfefc757e74bd94a083de08bda58f6caa57a
Jan Domingo <janaarondomingo@gmail.com>
2019-09-12 14:34:09 -0700

    Changed ^ character to & character

--- Assignments/One/getword.c
+++ Assignments/One/getword.c
@@ -5,7 +5,7 @@
 //  Course: CS570 Operating Systems
 //  Instructor Name: John Carroll
 //  Section: 1
-//  TODO: Due Date: September 4, 2019
+//  TODO: Due Date: September 16, 2019
 //  Assignment: Program 1
 //  File name: getword.c
 //  Compiler Version: XCode 10.2.1
@@ -33,7 +33,7 @@
                             //double backslash is used to represent a single backslash
 
 //This function returns either a 0 or a 1 depending on if the iochar detected is a metacharacter
-//Possible cases for metacharacers: '>', '>>', '>&', '>>^', '|', '#', '&'
+//Possible cases for metacharacers: '>', '>>', '>&', '>>&', '|', '#', '&'
 static int metaCharacterCheck(int iochar) {
     if (iochar == '>' ||  iochar == '|' || iochar == '#' || iochar == '&') {
         //ungetc(iochar, stdin);
@@ -44,7 +44,7 @@ static int metaCharacterCheck(int iochar) {
 
 //This function eturns the metacharacter word size and puts the metacharacters on the w pointer to output.
 //It is called "greedyAlgorithm" as takes the largest metacharacter possible when reading from left to right
-//(E.g. >>>^ returns a >> and >^. NOT > and >>^)
+//(E.g. >>>& returns a >> and >&. NOT > and >>&)
 //This function handles the case of detected metacharacters as well as all subsequent metacharacters
 //following the call to this function. As a result, any chars that is not a meta character while this function
 //is running is pushed back into the stdin stream for rerun in p0.c
@@ -63,14 +63,14 @@ static int greedyAlgorithm(int iochar, char *w, char *wstart) {
              metaCharWordSize++;
              iochar = getchar();
             
-            if (iochar == '^') {
+            if (iochar == '&') {
                 *w = iochar;
                 w++;
                 metaCharWordSize++;
                 iochar = getchar();
                 if (metaCharacterCheck(iochar) == NOT_META)
                     ungetc(iochar, stdin);
-                return metaCharWordSize;    //Return '>>^'
+                return metaCharWordSize;    //Return '>>&'
             }
             if (metaCharacterCheck(iochar) == NOT_META)
                 ungetc(iochar, stdin);
