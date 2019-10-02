9d018db609f55b4588cc8c05c8e86759af90d3be
Jan Domingo <janaarondomingo@gmail.com>
2019-10-02 02:53:06 -0700

    Fork function added, no finctionality yet. Added '>' flag case

--- Assignments/Two/p2.c
+++ Assignments/Two/p2.c
@@ -17,11 +17,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <unistd.h>
 #include "getword.h"
 
 #define MAX_ARGS 100
 #define MAX_WORD_LENGTH 255
 #define EMPTY 0
+#define NOT_SET 0
+#define SET 1
+
+//Global Variables
+int inputRedirectionFlag = NOT_SET;
+int outputRedirectionFlag = NOT_SET;
+int outputRedirectionAndAmpersandFlag = NOT_SET;
+int ampersandFlag = NOT_SET;
+
+char *nameOfInputFileRedirection[MAX_WORD_LENGTH];  //TODO: FIND A WAY TO MALLOC THIS
+char *nameOfOutputFileRedirection[MAX_WORD_LENGTH];  //TODO: FIND A WAY TO MALLOC THIS
 
 /************************************************************************************************************/
 /**********************************THIS IS THE PARSE FUNCTION************************************/
@@ -30,11 +42,32 @@
 //This will set appropriate flags when getword() encounters words that are metacharacters
 void parse(char argsList[MAX_ARGS][MAX_WORD_LENGTH], int argsCount) {
     
-    char *inputFileRedirectionName[MAX_WORD_LENGTH];
-    
+    //memset(*nameOfInputFileRedirection, EMPTY, MAX_WORD_LENGTH);    //Removes already created garbage
+
+    //This loop cycles through all of the arguments and compares it with flags that needs to be set
     for (int loopIteration = EMPTY; loopIteration < argsCount; loopIteration++) {
-        if (strcmp(argsList[loopIteration], "<")) {
-            *inputFileRedirectionName = argsList[loopIteration++]; //TODO: FIND A WAY TO MAKE SURE THIS EXISTS, IF NOT THEN PERROR AND ERROR MESSAGE
+        
+        //This block handes input redirection cases
+        if (strcmp(argsList[loopIteration], "<"))   {       //TODO: SET UP A WAY TO DETECT IF FIRST WORD IN CHAR IS A FILE NAME
+            
+            //If there are multiple input redirections then produce an error
+            if (inputRedirectionFlag == SET) {
+                perror("Error: multiple input redirections. Use only one"); //TODO: DOUBLE CHECK IF DOUBLE MULTIPLE INPUT IS OK. TRY TO FIGURE WHY THIS IS NOT WORKING
+            }
+            
+            inputRedirectionFlag = SET;
+            *nameOfInputFileRedirection = argsList[loopIteration++]; //Gets the next word in argument after <
+                                                                   //and saves it as the input file name
+            //TODO: FIND A WAY TO CHECK FIRST IF THE FILENAME EXISTS, IF NOT THEN PERROR AND ERROR MESSAGE
+        }
+        
+        if (strcmp(argsList[loopIteration], ">")) {
+            if (outputRedirectionFlag == SET) {
+                perror("Error: multiple input redirections. Use only one");
+            }
+            
+            outputRedirectionFlag = SET;
+            *nameOfOutputFileRedirection = argsList[loopIteration++];
         }
     }
     
@@ -55,10 +88,6 @@ void parse(char argsList[MAX_ARGS][MAX_WORD_LENGTH], int argsCount) {
     //TODO: Use the argsList array as stdin for getword function
     //Get the word from the arrray that needs to be copied into here
 
-    //Run getword through the argslist
-    for(;;) {
-        argsLength = getword(argsListString);
-        if (argsLength == -1) break;
     }
     
     
@@ -69,7 +98,6 @@ void parse(char argsList[MAX_ARGS][MAX_WORD_LENGTH], int argsCount) {
     
     
     
-    }
 
 /************************************************************************************************************/
 /**********************************THIS IS THE MAIN FUNCTION**************************************/
@@ -85,7 +113,9 @@ int main(int argc, char *argv[])
     argv[0] = "./p2";
     argv[1] = "<";
     argv[2] = "hello";
-    argc = 2;
+    argv[3] = "<";
+    argv[4] = "world";
+    argc = 5;
     char argsList[MAX_ARGS][MAX_WORD_LENGTH];
     
     //Clears the memory in argsList by setting the elements from the character pointer up to the total size
@@ -106,6 +136,10 @@ int main(int argc, char *argv[])
     
     //TODO: After putting words into an array, put them into the parse function to set flags
     parse(argsList, argc);
+    fork();
+    wait(NULL); //CHECK IF NULL IS THE RIGHT PARAMETER
+    
+    
     
     
 
