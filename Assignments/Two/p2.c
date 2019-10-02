2e0c79b6a8d786d4bac7c39f1ba1fa5d4c823c41
Jan Domingo <janaarondomingo@gmail.com>
2019-10-01 20:01:58 -0700

    argsList array working and storing values of the command line arguments

--- Assignments/Two/p2.c
+++ Assignments/Two/p2.c
@@ -19,12 +19,12 @@
 #define MAX_ARGS 100
 #define MAX_WORD_LENGTH 255
 
-char argsArray[MAX_ARGS][MAX_WORD_LENGTH];
-char stringArray[MAX_WORD_LENGTH]; //This array gets inputted into the getword function inside parse().
+char argsList[MAX_ARGS][MAX_WORD_LENGTH];
+char s[MAX_WORD_LENGTH]; //This array gets inputted into the getword function inside parse().
 
 //This function is responsible for the syntactic analysis
 //This will set appropriate flags when getword() encounters words that are metacharacters
-int parse(int argumentCount, char *argumentValue) {
+void parse() {
     
     //TODO: Store words from arguments into arrays here
     //TODO: Use getword to separate the words
@@ -34,38 +34,36 @@ int parse(int argumentCount, char *argumentValue) {
         //Create an array that houses the command line arguments
         //Get the word from the arrray that needs to be copied into here
         
+    int c;
     
-    int c = getword(stringArray);
-    printf("%d, %s", c, stringArray);
-    
-    
-    
-    
-    return 0;
-}
+    for(;;) {
+        (void) printf("n=%d, s=[%s]\n", c = getword(s), s);
+        if (c == -1) break;
+        }
+    }
 
 int main(int argc, char *argv[])
 {
-    char *argvCopy = *argv;
-    parse(argc, argvCopy);
+    /*********************THIS SECTION COPIES THE COMMAND LINE ARGUMENTS INTO THE ARGSLIST ARRAY****************************/
+    //This outer for loop increments after each individual argument word
+    for (int argumentIndex = 0; argumentIndex < argc; argumentIndex++) {
+        int length = (int)strlen(argv[argumentIndex]);
+        
+        //This inner for loop increments after each character of the current argument word
+        for (int argumentIndexChar = 0; argumentIndexChar <= length; argumentIndexChar++) {
+            argsList[argumentIndex][argumentIndexChar] = argv[argumentIndex][argumentIndexChar];
+        }
+    }
+    
+    //TODO: After putting words into an array, put them into the parse function to set flags
+    
+    
+    
+    parse();
     //TODO: STEP 1 - Get name of file to open in argv[1]
     //TODO: STEP 2 - Execute/open file of argv[1]
     //TODO: STEP 3 - Issue error message if word not there
     
-    if (strcmp(argv[1], ">") == 0) {
-        
-    }
-    printf("File name %s\n", argv[1]);
-    
-    
-    
-    
-    
-    
-    //for(;;) {
-    printf("%%/%% \n");     //The user sees '%/% ' when input is needed
-    //}
-
     
     return(EXIT_SUCCESS);
 }
