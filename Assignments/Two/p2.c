2426f68e279479561685a113b5d380f9e38af38e
Jan Domingo <janaarondomingo@gmail.com>
2019-10-03 23:39:01 -0700

    Cleaned up and deleted unncessary comments

--- Assignments/Two/p2.c
+++ Assignments/Two/p2.c
@@ -30,10 +30,11 @@
 #define EMPTY 0
 #define NOT_SET 0
 #define FILE_EXISTS 0
+#define CMD_INDEX 0
+#define CHILD 0
 #define SET 1
 #define MATCH 1
-#define CMD_INDEX 0
-
+#define FORK_FAILED -1
 #define TERMINATED -1
 
 
@@ -51,130 +52,7 @@ char *nameOfOutputFileRedirection[MAX_WORD_LENGTH];  //TODO: FIND A WAY TO MALLO
 /************************************************************************************************************/
 //This function is responsible for the syntactic analysis
 //This will set appropriate flags when getword() encounters words that are metacharacters
-
-/******
- 
-void parse(char argsList[MAX_ARGS][MAX_WORD_LENGTH], int argsCount) {
-    
-    //memset(*nameOfInputFileRedirection, EMPTY, MAX_WORD_LENGTH);    //Removes already created garbage
-    //This loop cycles through all of the arguments and compares it with flags that needs to be set
-    for (int loopIteration = EMPTY; loopIteration < argsCount; loopIteration++) {
-        
-        //This block handes input redirection cases
-        if (strcmp(argsList[loopIteration], "<"))   {       //TODO: SET UP A WAY TO DETECT IF FIRST WORD IN CHAR IS A FILE NAME
-            
-            //If there are multiple input redirections then produce an error
-            if (inputRedirectionFlag == SET) {
-                perror("Error: multiple input redirections. Use only one"); //TODO: DOUBLE CHECK IF DOUBLE MULTIPLE INPUT IS OK. TRY TO FIGURE WHY THIS IS NOT WORKING
-            }
-            
-            inputRedirectionFlag = SET;
-            *nameOfInputFileRedirection = argsList[loopIteration++]; //Gets the next word in argument after <
-                                                                   //and saves it as the input file name
-            //TODO: FIND A WAY TO CHECK FIRST IF THE FILENAME EXISTS, IF NOT THEN PERROR AND ERROR MESSAGE
-        }
-        
-        if (strcmp(argsList[loopIteration], ">")) {
-            if (outputRedirectionFlag == SET) {
-                perror("Error: multiple input redirections. Use only one");
-            }
-            
-            outputRedirectionFlag = SET;
-            *nameOfOutputFileRedirection = argsList[loopIteration++];
-        }
-    }
-    
-    
-    
-    
-    
-    
-    
-    //This is an empty array that is used to store argsList values when being called in the getword function
-    //char argsListString[MAX_WORD_LENGTH];
-    char *argsListString = malloc(MAX_WORD_LENGTH / sizeof(char));
-    
-    int argsLength;
-    
-    
-    //TODO: Use getword to separate the words
-    //TODO: Use the argsList array as stdin for getword function
-    //Get the word from the arrray that needs to be copied into here
-    }
- 
- *****/
-
-/**void parse2(char cmd[], char *parameters[]) {
-    //char getwordArray[1024];
-    char inputLine [1024];
-    char argsList[MAX_ARGS][MAX_WORD_LENGTH];
-    char *array[100];
-    char *pointerCharacter = malloc (1024 * sizeof(char));
-    char *arrayCopyPtr;
-    int index = 0;
-    int i = 0;
-    int j = 0;
-    
-    memset(*argsList, EMPTY, (MAX_ARGS * MAX_WORD_LENGTH));
-    
-    //Reading in one line
-    for (;;) {
-        int c = fgetc (stdin);
-        inputLine[index++] = (char) c;  //stdin in the command line input
-        if (c == '\n') {
-            break;
-        }
-    }
-    
-    //Copies the line and parses into individual individual words
-    pointerCharacter = strtok (inputLine, "\n");
-    int wordIndex = 0;
-    int charIndex = 0;
-    
-    //FIGURE OUT WHAT THIS DOES
-    if (index == 1)
-        return;
-    
-    //pointerCharacter = strtok (inputLine, "\n");
-    
-    //Parse the line into words
-    while (pointerCharacter != NULL) {
-        array[i++] = strdup(pointerCharacter);
-        pointerCharacter = strtok(NULL, "\n");
-    }
-    
-    //First word is the command
-    strcpy (cmd, array[0]);
-    
-    //Others are parameters
-    for (j = 0; j < i; j++)
-        parameters[j] = array[j];
-    parameters[j] = NULL;
-    
-    if (access(cmd, F_OK) != -1) {
-        printf("File exists\n");
-    }
-} **/
-    
-
-    //char *parameterArray[MAX_WORD_LENGTH], *parameterPointer;
-    
-    
-    /*
-    if (getword(line) == -1)
-        exit(1);
-    
-    while (parameterPointer != NULL) {
-        parameterArray[i++] = strdup (parameterPointer);
-        parameterPointer = strtok(NULL, "\n");
-    } */
-
-/************************************************************************************************************/
-/**********************************THIS IS THE PARSE FUNCTION************************************/
-/************************************************************************************************************/
-//This function is responsible for the syntactic analysis
-//This will set appropriate flags when getword() encounters words that are metacharacters
-void parse3(char *argsLine, char command[], char *arrayOfArgsLinePtr[]) {
+void parse(char *argsLine, char command[], char *arrayOfArgsLinePtr[]) {
 
     char arrayOfArgsLine[MAX_CMD_LENGTH][MAX_WORD_LENGTH];  //TODO: CHECK IF A 2D ARRAY HERE IS REALY NECESSARY
     memset(*arrayOfArgsLine, EMPTY, (MAX_ARGS * MAX_WORD_LENGTH));  //Clears the array of garbage values
@@ -207,6 +85,7 @@ void parse3(char *argsLine, char command[], char *arrayOfArgsLinePtr[]) {
         
         //This block handles the case of the metacharacter '<'. If detected, SET the inputRedirectionFlag
         //TODO: CHECK INPUT7 OR INPUT8 FOR FURTHER TEST CASES OF UNIX REDIRECTION
+        //TODO: FIX THIS REDIRECTION INPUT AS IT IS TRUE EVERYTIME IN ALL CASES
         if (strcmp(arrayOfArgsLine[loopIteration], "<")) {
             //If the inputRedirectionFlag has already been set from a prior call then print an error
             if (inputRedirectionFlag == SET) {
@@ -217,57 +96,19 @@ void parse3(char *argsLine, char command[], char *arrayOfArgsLinePtr[]) {
                 inputFileName = arrayOfArgsLine[++loopIteration];
                 parameterPtr = *(arrayOfArgsLine + 1);  //Points to the same place as the inputFileName
                 
-                //TODO: ONLY USED FOR DEBUGGING
-                /****
-                char cwd[MAX_WORD_LENGTH];
-                getcwd(cwd, sizeof(cwd));
-                chdir("/Users/jandomingo/Dev/CS570/Assignments/Two/");
-                getcwd(cwd, sizeof(cwd));
-                strcat(cwd, "/");
-                strcat(cwd, inputFileName);
-                
-                printf("Current file looking at: %s\n", cwd);
-                //printf("Current path: %s", getenv("PATH"));
-                //TODO: UP TO HERE
-                
-                //If the filename exists then set the redirection flag to SET
-                //TODO: ACCESS NOT WORKING PROPERLY, SAYS UNEXISTING FILE EXISTS
-                if(access(cwd, F_OK) == FILE_EXISTS) {
-                   inputRedirectionFlag = SET;
-                    printf("FILE EXISTS");  //TODO: REMOVE THIS WHEN SOLVED. THIS SHOULD NOT APPER IN FINAL VERSION
-                } else {
-                    perror("File cannot be located");
-                }
-            }****/
         }
         
-        //This block handles all other commands (i.e. when the user did not input a metacharacter)
+        //This block handles all other commands (i.e. when the user did not input a metacharacter) /TODO: UPDATE THIS DESCRIPTION
         //In this case, the user could be entering a command to be executed in execvp or the first word
         //has to be a file to read in.
         //TODO: Add more flags as needed, double check to see if ampersandFlag NOT_SET is appropraite here
-        if (inputRedirectionFlag == NOT_SET && outputRedirectionFlag== NOT_SET && ampersandFlag == NOT_SET) {
-            strcpy(command, arrayOfArgsLine[CMD_INDEX]);
-            //*parameters = arrayOfArgsLine[1];
-            //strcpy(*parameters, *arrayOfArgsLine);
-            
-        }
-            break;
-        
-        
-    }
-    
-    
-    
-        /*
-        while (getwordFnResult != 0 || getwordFnResult != -1)
-        {
-            if (getwordFnResult > 0) {
-                printf("%s", argsLine);
+            if (inputRedirectionFlag == NOT_SET &&
+                outputRedirectionFlag == NOT_SET &&
+                ampersandFlag == NOT_SET) {
+                strcpy(command, arrayOfArgsLine[CMD_INDEX]);
             }
-        
+            break;
         }
-         */
-        
     }
 }
 
@@ -289,14 +130,15 @@ int main(int argc, char *argv[])
 {
     char *argsLine = malloc(MAX_CMD_LENGTH * sizeof(char));
     char *command = malloc(MAX_CMD_LENGTH * sizeof(char));
-    //char *parameters = malloc(MAX_CMD_LENGTH * sizeof(char));
     //Parameters is the same as argsline but is instead passed into parse() as an array of pointers to char
-    char *parameters[MAX_ARGS] = {EMPTY};
+    char *parameters[MAX_ARGS] = {NULL};
     
-
+    
+    /**********THIS SECTION HANDLES WHEN THE USER INPUTS 'cd'************/
+    
     for(;;) {
-        printf("%%1%% ");
-        parse3(argsLine, command, parameters);
+        printf("%%1%% \n");
+        parse(argsLine, command, parameters);
         
         //execvp (command, parameters);
         //char *name[5];
@@ -309,69 +151,36 @@ int main(int argc, char *argv[])
         //execvp (command, name);
         //execvp (command, &parameters);
         
-        if (fork() == 0) {
+        pid_t pid = fork();
+        if (pid == FORK_FAILED) {
+            perror("Fork Failed");
+            exit(9);
+        }
+        if (pid == CHILD) {
             execvp (command, parameters);
             //execvp("echo", name);
             //execvp (command, parameters);  //TODO: THIS IS THE NEXT STEP!!!!!! MAKE SURE THAT PARAMETER VALUES ARE BEING PASSED IN PROPERLY
             //fflush(stdout);
+            perror("execvp failed");
+            exit(1);
             
-            
-            exit(9);
+            //exit(9);
             
         } else {
-            wait (NULL);
-            printf("%s", "parent");
+            int status;
+            pid = wait(&status);
+            if (WIFEXITED(status)) {
+                printf("pid %d exited with status %d\n", pid, WEXITSTATUS(status));
+            } else {
+                printf("pid %d exited abnormally\n", pid);
+            }
+            
+            printf("%s", "parent\n");
         }
-        
-        exit(0);
-        
-        
+        printf("p2 terminated.\n");
+        exit(1);
     }
+    
     return (EXIT_SUCCESS);
     
-    
-    
-    
-    
-    
-    
-    /*
-    argv[0] = "./p2";
-    argv[1] = "<";
-    argv[2] = "hello";
-    argv[3] = "<";
-    argv[4] = "world";
-    argc = 5;
-    char argsList[MAX_ARGS][MAX_WORD_LENGTH];
-    
-    //Clears the memory in argsList by setting the elements from the character pointer up to the total size
-    //of argsList (25500 bytes) to EMPTY (0). This is a workaround for a two dimensional array malloc.
-    memset(*argsList, EMPTY, (MAX_ARGS * MAX_WORD_LENGTH));
-    
-    //This section copies the arguments from the command line input and stores it into the argsList array
-    //This outer for loop increments after reading off a word in the command line argument
-    for (int argumentIndex = 1; argumentIndex <= argc; argumentIndex++) {
-        int lengthOfArgument = (int)strlen(argv[argumentIndex]);    //Amount of chars in an argument
-        
-        //This inner for loop increments after each character of the current argument word
-        for (int argumentIndexChar = 0; argumentIndexChar <= lengthOfArgument; argumentIndexChar++) {
-            argsList[argumentIndex][argumentIndexChar] = argv[argumentIndex][argumentIndexChar];
-        }
-    }
-    
-    
-    //TODO: After putting words into an array, put them into the parse function to set flags
-    parse(argsList, argc);
-    fork();
-    wait(NULL); //CHECK IF NULL IS THE RIGHT PARAMETER
-    
-    
-    
-    
-    //TODO: STEP 1 - Get name of file to open in argv[1]
-    //TODO: STEP 2 - Execute/open file of argv[1]
-    //TODO: STEP 3 - Issue error message if word not there
-    
-    
-    return(EXIT_SUCCESS);*/
 }
