cdaa0ade26282358bd00fc4c92a3703654bab187
Jan Domingo <janaarondomingo@gmail.com>
2019-10-21 16:05:16 -0700

    Build succeededing in Xcode. Renamed main in p3robot.c need to change back when finished

--- Assignments/Three/p3helper.c
+++ Assignments/Three/p3helper.c
@@ -1,3 +1,18 @@
+//
+//  Name: Jan Domingo
+//  RedID: 820092657
+//  Class Account: CSSC0034
+//  Course: CS570 Operating Systems
+//  Instructor Name: John Carroll
+//  Section: 1
+//  TODO: UPDATE DUE DATE!! Due Date: TBD
+//  Assignment: Program 2
+//  File name: p2.c
+//  Compiler Version: XCode 11.0 (11A420a)
+//
+//  TODO: ADD DESCRIPTION OF THIS PROGRAM
+
+
 /* p3helper.c
    Program 3 assignment
    CS570
