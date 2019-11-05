054e002b1cc77c3d78b83de01df0bc6f49d78eb9
Jan Domingo <janaarondomingo@gmail.com>
2019-11-05 01:30:14 -0800

    Triangle printing working. Only problem is that the processes are all going in at once in the for loop. Maybe add another semaphore?

--- Assignments/Three/p3helper.c
+++ Assignments/Three/p3helper.c
@@ -28,7 +28,15 @@ char semaphoreMutx[SEMNAMESIZE];    //Name of sempahore
 
 int fd;
 int count;
-int waiting = 0;
+
+int fd2;
+int rowprint;
+
+int fd3;
+int printcount;
+
+int fd4;
+int maxpeakhit;
 
 /* General documentation for the following functions is in p3.h
    Here you supply the code, and internal documentation:
@@ -41,9 +49,23 @@ void initStudentStuff(void) {
          printf("SEMPAPHORE NAME: %s\n", semaphoreMutx);
 
          CHK(fd = open("countfile", O_RDWR|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR));
+         CHK(fd2 = open("rowprintfile",O_RDWR|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR));
+         CHK(fd3 = open("printcountfile", O_RDWR|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR)); //TODO: CHECK IF THIS LINE IS NEEDED
+         CHK(fd4 = open("maxpeakhitfile", O_RDWR|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR)); //TODO: CHECK IF THIS LINE IS NEEDED
+         
          count = 0;
-         CHK(lseek(fd,0,SEEK_SET));
+         rowprint = 1;
+         printcount= 0;
+         maxpeakhit = 0;
+         
+         CHK(lseek(fd,0,SEEK_SET)); //Move the read/write file pointer position to the beginning of the file
+         CHK(lseek(fd2,0,SEEK_SET)); //Move the read/write file pointer position to the beginning of the file
+         CHK(lseek(fd3,0,SEEK_SET)); //Move the read/write file pointer position to the beginning of the file
+         CHK(lseek(fd4,0,SEEK_SET)); //Move the read/write file pointer position to the beginning of the file
          assert(sizeof(count) == write(fd,&count,sizeof(count)));
+         assert(sizeof(rowprint) == write(fd2, &rowprint, sizeof(rowprint)));
+         assert(sizeof(printcount) == write(fd3, &printcount, sizeof(printcount)));
+         assert(sizeof(maxpeakhit) == write(fd4, &maxpeakhit, sizeof(maxpeakhit)));
          
         printf("INITIALIZED: %d\n", getpid());
          CHK(sem_post(pmutx));
@@ -52,15 +74,17 @@ void initStudentStuff(void) {
          //If process gets here before countfile is initialized, then wait on semaphore to finish
          if (access("countfile", F_OK) == -1) {
              //if sem_wait is placed here then only once process will be printing out
-             sleep(1);  //TODO: DELETE THIS WHEN PROGRAM IS FINISHED. FIND A WAY TO ELIMINATE RACE CONDITIONS
+             sleep(random()%2);  //TODO: DELETE THIS WHEN PROGRAM IS FINISHED. FIND A WAY TO ELIMINATE RACE CONDITIONS
              //printf("NOT CREATED YET!!!");
              //CHK(fd = open("countfile", O_RDWR));
          }
          
          printf("NOT THE INITIALIZER: %d\n", getpid());
-         pmutx = sem_open(semaphoreMutx, O_RDWR, 1);
+         pmutx = sem_open(semaphoreMutx, O_RDWR);   //TODO: Initialize this to 1?
          CHK(fd = open("countfile", O_RDWR));
-         
+         CHK(fd2 = open("rowprintfile", O_RDWR)); //TODO: CHECK IF THIS LINE IS NEEDED
+         CHK(fd3 = open("printcountfile", O_RDWR)); //TODO: CHECK IF THIS LINE IS NEEDED
+         CHK(fd4 = open("maxpeakhitfile", O_RDWR)); //TODO: CHECK IF THIS LINE IS NEEDED
 
      }
 }
@@ -81,27 +105,88 @@ void placeWidget(int n) {
     
     CHK(lseek(fd,0,SEEK_SET));
     assert(sizeof(count) == read(fd, &count, sizeof(count)));
+    
+    CHK(lseek(fd2,0,SEEK_SET));
+    assert(sizeof(rowprint) == read(fd2, &rowprint, sizeof(rowprint)));
+    
+    CHK(lseek(fd3,0,SEEK_SET));
+    assert(sizeof(printcount) == read(fd3, &printcount, sizeof(printcount)));
+    
+    CHK(lseek(fd4,0,SEEK_SET));
+    assert(sizeof(maxpeakhit) == read(fd4, &maxpeakhit, sizeof(maxpeakhit)));
+    
     count++;
     
-    printf("COUNT: %d\n", count);
+    //printf("COUNT: %d\n", count);
     
-    if (count == (nrRobots * quota)) {
+  if (count == (nrRobots * quota)) {
+        printf("COUNT: %d\n", count);
+        printf("ROWPRINT: %d\n", rowprint);
+        printf("PRINTCOUNT: %d\n", printcount);
+        printf("MPH: %d\n", maxpeakhit);
         printeger(n);
         printf("F\n");
         CHK(close(fd));
         CHK(unlink("countfile"));
+        CHK(close(fd2));
+        CHK(unlink("rowprintfile"));
+        CHK(close(fd3));
+        CHK(unlink("printcountfile"));
+        CHK(close(fd4));
+        CHK(unlink("maxpeakhitfile"));
         CHK(sem_close(pmutx));
         CHK(sem_unlink(semaphoreMutx));
         
     } else {
-        printeger(n);
-        printf("N\n");
         
-        CHK(lseek(fd,0,SEEK_SET));
+        printf("COUNT:%d\n ", count);
+        printeger(n);
+        
+        int i;
+        for (i = 1; i <= rowprint; i++) {
+            printcount++;
+        }
+        
+        if (i == rowprint) {
+            printeger(n);
+            printf("N\n");
+            fflush(stdout);
+        } else {
+            printeger(n);
+            fflush(stdout);
+        }
+        
+        if (printcount > ((nrRobots * quota) / 2)) {
+            //Max peak on triangle, start decerementing rowprint
+            maxpeakhit = 1;
+            //printf("COUNT MPH:%d ", count);
+            //printf("PRINTCOUNT MPH:%d ", printcount);
+            //printf("MAX PEAK HIT:%d\n", maxpeakhit);
+        }
+        
+        if (maxpeakhit == 0) {
+            rowprint++;
+        } else if (maxpeakhit == 1){
+            rowprint--;
+        }
+        
+        
+        CHK(lseek(fd, 0, SEEK_SET));
         assert(sizeof(count) == write(fd, &count, sizeof(count)));
+        CHK(lseek(fd2, 0, SEEK_SET));
+        assert(sizeof(rowprint) == write(fd2, &rowprint, sizeof(rowprint)));
+        CHK(lseek(fd3,0,SEEK_SET));
+        assert(sizeof(printcount) == write(fd3, &printcount, sizeof(printcount)));
+        CHK(lseek(fd4,0,SEEK_SET));
+        assert(sizeof(maxpeakhit) == write(fd4, &maxpeakhit, sizeof(maxpeakhit)));
+        
         fflush(stdout);
+        
         CHK(sem_post(pmutx));
+        
     }
+    
+    
 }
 
 /* If you feel the need to create any additional functions, please
